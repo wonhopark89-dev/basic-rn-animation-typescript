a5eedc107b61ad2c38f62d595bcd641616b7a613
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-17 23:43:47 +0900

    animation timing and spring

--- App.tsx
+++ App.tsx
@@ -3,11 +3,19 @@ import { Animated, Text, TouchableOpacity, View } from 'react-native';
 
 const App = () => {
 	const [value, setValue] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
+	const [marginLeft, setMarginLeft] = useState(new Animated.Value(0));
 
 	const moveBall = () =>
-		Animated.timing(value, {
+		Animated.spring(value, {
 			toValue: { x: 100, y: 200 },
-			duration: 1000,
+			// duration: 1000,
+			useNativeDriver: false,
+		}).start();
+
+	const moveLeft = () =>
+		Animated.spring(marginLeft, {
+			toValue: 200,
+			// duration: 1000,
 			useNativeDriver: false,
 		}).start();
 
@@ -23,8 +31,20 @@ const App = () => {
 					}}
 				/>
 			</Animated.View>
+			<Animated.View
+				style={{
+					width: 100,
+					height: 100,
+					borderRadius: 100 / 2,
+					backgroundColor: 'cyan',
+					marginLeft: marginLeft,
+				}}
+			/>
 			<TouchableOpacity onPress={moveBall}>
 				<Text>Move ~ </Text>
+			</TouchableOpacity>
+			<TouchableOpacity onPress={moveLeft}>
+				<Text>Move Left ~</Text>
 			</TouchableOpacity>
 		</View>
 	);
